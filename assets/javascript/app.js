55f2be984d4391f6e1ad2c1e8e9a56c7438d6a1e
Andres Aguaiza <andres.aguaiza@dewilmaaguaizam.local>
2019-06-20 16:35:10 -0400

    adding form needed for questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,30 @@
 
 //Sets the interval to time the carousel;
 $('.carousel').carousel({
-  interval: 2000
+  interval: 3000
 });
 
 
 //Starts cycling the carousel
 $('.carousel').carousel('cycle');
+
+
+// Going to use this timeout function to control the carousel to make all updates in one swooop
+// NOTE the carousel can be controled with it's own Jquery methods but it might make triggering events a bit more complicated
+// than simply using a timout function
+setTimeout(function(){ alert("Hello"); }, 3000);
+
+//This will handle all logic for switching slides and...
+//Determine if the guess was correct
+//update score
+//update any trackers that will be needed
+
+$( "input" ).on( "click", function() {
+console.log($( "input:checked" ).val() + " is checked!");
+});
+
+
+
+var nextSlide = function(){
+
+}
