fd02e57d6d1af2f243079c35dea184064a3f4618
Andes Aguaiza <andresagu2595@gmail.com>
2019-06-20 23:38:43 -0400

    changed checkbox to select button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,61 @@
 
 //Sets the interval to time the carousel;
-$('.carousel').carousel({
-  interval: 3000
-});
+// $('.carousel').carousel({
+//   interval: 3000
+// });
+
+//Stores array of correct answers
+var answers = [];
+
+//global vars to count the game stats
+var correctCount = 0;
+var incorrectCount = 0;
+var blankCount = 0;
 
 
 //Starts cycling the carousel
-$('.carousel').carousel('cycle');
+//$('.carousel').carousel('cycle');
 
 
 // Going to use this timeout function to control the carousel to make all updates in one swooop
 // NOTE the carousel can be controled with it's own Jquery methods but it might make triggering events a bit more complicated
 // than simply using a timout function
-setTimeout(function(){ alert("Hello"); }, 3000);
+//This will alert every 10 seconds
+//switching pages on the carousel and determining whether there was a win or loss
+setInterval(function(){
 
-//This will handle all logic for switching slides and...
-//Determine if the guess was correct
-//update score
-//update any trackers that will be needed
+  //reads input at set interval
 
-$( "input" ).on( "click", function() {
-console.log($( "input:checked" ).val() + " is checked!");
-});
+  var input = $( "input:checked" ).val();
+  console.log( input + " is checked!");
+
+  $(":radio").prop('checked', false);
+
+  //$('.btn-group').find('label').removeClass('active').end().find('[type="radio"]').prop('checked', false);
+
+  $('.carousel').carousel('next');
+
+
+
+
+
+
+
+
+ }, 10000);
+
+
 
 
 
 var nextSlide = function(){
 
 }
+
+
+//FIRES WHEN TIMOUT EXCECUTES SLIDE FOR CAROUSEL
+$('.carousel').on('slide.bs.carousel', function () {
+
+  console.log("executing slide logic");
+  // do something…
+})
